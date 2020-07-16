76dd2d8b77b72949e2efd677799cd89a92f3dfca
Erik Poulsen <33035080+ErikPDev@users.noreply.github.com>
2020-07-16 14:55:33 +0300

    Rebuild the branch

---
+++ README.md
@@ -0,0 +1,26 @@
+# Sudo
+[![PoggitCI](https://poggit.pmmp.io/shield.state/Sudo)](https://poggit.pmmp.io/p/Sudo)
+[![Discord](https://discordapp.com/api/guilds/717993428423671818/embed.png)](https://discord.gg/ZyUUy57)
+![GitHub All Releases](https://img.shields.io/github/downloads/ErikX-PMMP/Sudo/total)
+![GitHub](https://img.shields.io/github/license/ErikX-PMMP/Sudo)
+[![HitCount](http://hits.dwyl.com/ErikX-PMMP/Sudo.svg)](http://hits.dwyl.com/ErikX-PMMP/Sudo)
+
+Time to sudo you!
+
+## NOTE
+This plugin is designed for **[PocketMine-MP](https://github.com/pmmp/PocketMine-MP)** only and we do not support any other variations, forks or spoons.
+Compatibility with unofficial variants can occasionally be found, but **do not expect support if you are using anything other than PocketMine-MP**.
+
+# Basic documentation
+## Commands:
+- /sudo player message/command - Send a message or run command as another player!
+### Examples:
+- /sudo ErikX Hi There!
+- /sudo ErikX /say HI there!
+
+## Permissions:
+
+| Permission    | Default       | Description                  |
+| ------------- |:-------------:| ----------------------------:|
+| sudo.use      |      OP       | Permission to sudo a player! |
+
---
+++ src/ErikX/sudo/Main.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace ErikX\sudo;
+
+use pocketmine\plugin\PluginBase;
+
+// Event
+use pocketmine\event\player\PlayerJoinEvent;  //This is the event 
+use pocketmine\Player;
+use pocketmine\Server;
+use pocketmine\event\Listener;
+
+use pocketmine\command\Command;
+
+use pocketmine\command\CommandSender;
+use pocketmine\utils\TextFormat;
+
+
+class Main extends PluginBase implements Listener { //Added "implements Listener" because of the Listener event
+
+    public function onEnable() {
+        $this->getServer()->getPluginManager()->registerEvents($this,$this); // This is the new line 
+        $this->saveDefaultConfig(); // Saves config.yml if not created.
+        $this->reloadConfig(); // Fix bugs sometimes by getting configs values
+      }
+    public function onLoad(){
+      $this->reloadConfig(); 
+    }
+    public function onCommand(CommandSender $sender, Command $cmd, string $label, array $args) : bool {
+      $prefix = TextFormat::GREEN . "[" . TextFormat::YELLOW . "Sudo" . TextFormat::GREEN . "] ";
+      $usage = $this->getConfig()->get("usage");
+      $notfound = $this->getConfig()->get("notfound");
+      switch (strtolower($cmd->getName())) {
+        case "sudo":
+          if (count($args) < 2) {
+            $sender->sendMessage($prefix . $usage);
+            return true;
+        }
+        $player = $this->getServer()->getPlayer(array_shift($args));
+        if ($player instanceof Player) {
+            //$this->getServer()->dispatchCommand($player, trim(implode(" ", $args)));
+            $player->chat(trim(implode(" ", $args)));
+            return true;
+        } else {
+            $sender->sendMessage($prefix. $notfound);
+            return true;
+
+        }
+      }
+      return true;
+    }
+  
+}
