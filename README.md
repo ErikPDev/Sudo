814ff150af46bfab258755f50cbe4670249a076b
Erik <33035080+ErikPDev@users.noreply.github.com>
2023-07-08 18:51:27 +0300

    Support PM5

--- README.md
+++ README.md
@@ -4,9 +4,6 @@
 
 Time to sudo you!
 
-## NOTE
-This plugin is designed for **[PocketMine-MP](https://github.com/pmmp/PocketMine-MP)** only and we do not support any other variations, forks or spoons.
-Compatibility with unofficial variants can occasionally be found, but **do not expect support if you are using anything other than PocketMine-MP**.
 
 # Basic documentation
 ## Commands:
--- src/ErikX/sudo/Main.php
+++ src/ErikX/sudo/Main.php
@@ -3,13 +3,9 @@
 namespace ErikX\sudo;
 
 use pocketmine\plugin\PluginBase;
-
 use pocketmine\player\Player;
-use pocketmine\Server;
 use pocketmine\event\Listener;
-
 use pocketmine\command\Command;
-
 use pocketmine\command\CommandSender;
 use pocketmine\utils\TextFormat;
 
@@ -24,23 +20,47 @@ class Main extends PluginBase implements Listener {
     public function onLoad() : void {
       $this->reloadConfig();
     }
-    public function onCommand(CommandSender $sender, Command $cmd, string $label, array $args) : bool {
+
+    /**
+     * Sourced from PocketMine Source Code
+     * @param string $name
+     * @return Player|null
+     */
+    private function getPlayerByPrefix(string $name) : ?Player{
+        $found = null;
+        $name = strtolower($name);
+        $delta = PHP_INT_MAX;
+        foreach($this->getServer()->getOnlinePlayers() as $player){
+            if(stripos($player->getName(), $name) === 0){
+                $curDelta = strlen($player->getName()) - strlen($name);
+                if($curDelta < $delta){
+                    $found = $player;
+                    $delta = $curDelta;
+                }
+                if($curDelta === 0){
+                    break;
+                }
+            }
+        }
+        return $found;
+    }
+
+    public function onCommand(CommandSender $sender, Command $command, string $label, array $args) : bool {
       $prefix = TextFormat::GREEN . "[" . TextFormat::YELLOW . "Sudo" . TextFormat::GREEN . "] ";
       $usage = $this->getConfig()->get("usage");
       $notfound = $this->getConfig()->get("notfound");
-      switch (strtolower($cmd->getName())) {
-        case "sudo":
-          if (count($args) < 2) {
-            $sender->sendMessage($prefix . $usage);
-            return true;
+        if (strtolower($command->getName()) == "sudo") {
+            if (count($args) < 2) {
+                $sender->sendMessage($prefix . $usage);
+                return true;
+            }
+            $player = $this->getPlayerByPrefix(array_shift($args));
+            if ($player instanceof Player) {
+                $player->chat(trim(implode(" ", $args)));
+            } else {
+                $sender->sendMessage($prefix . $notfound);
+            }
         }
-        $player = $this->getServer()->getPlayerByPrefix(array_shift($args));
-        if ($player instanceof Player) {
-            $player->chat(trim(implode(" ", $args)));
-        } else {
-            $sender->sendMessage($prefix. $notfound);
-        }
-      }
       return true;
     }
 
